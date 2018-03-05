5f860364ebedf35aa10eb1e41e9064b773f697c2
2bukam38 <mashood-bukari@hotmail.co.uk>
2018-03-05 12:57:33 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a2bukam38/fileio/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.a2bukam38.fileio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.save)
+        {
+            // react to the menu item being selected...
+            return true;
+        }
+        else if(item.getItemId() == R.id.load)
+        {
+            // react to the menu item being selected...
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
